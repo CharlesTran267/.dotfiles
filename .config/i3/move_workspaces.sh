b5b58a50d67c0efe80e6ade35dfe5d79c861a1c1
Charles Tran <charlestran267@gmail.com>
2024-12-16 20:49:13 +0800

    update move_workspaces script

--- .config/i3/move_workspaces.sh
+++ .config/i3/move_workspaces.sh
@@ -6,26 +6,25 @@ socket_path="/run/user/1000/i3/$(ls -t /run/user/1000/i3/ | awk '{print $1}' | g
 # Get list of workspaces
 WORKSPACES=$(i3-msg --socket $socket_path -t get_workspaces | jq -r '.[] | .name')
 
-# Get focused workspace
-FOCUSED_WORKSPACE=$(i3-msg --socket $socket_path -t get_workspaces | jq -r '.[] | select(.focused==true) | .name')
-
 # Get list of visible workspaces
 VISIBLE_WORKSPACES=$(i3-msg --socket $socket_path -t get_workspaces | jq -r '.[] | select(.visible==true) | .name')
+
+# Get focused monitor
+FOCUSED_MONITOR=$(i3-msg --socket $socket_path -t get_workspaces | jq -r '.[] | select(.focused==true) | .output')
 
 for workspace in $WORKSPACES; do
     echo "Moving workspace $workspace to output right"
     i3-msg --socket $socket_path [workspace=$workspace] move workspace to output right
 done
 
-# Focus on the workspace that was visible but not focused before first. This is to ensure all previously-visible are still visible and previously-focused are still focused
+# Focus on the workspace that was visible to ensure all previously-visible are still visible.
 for workspace in $VISIBLE_WORKSPACES; do
-    if [ "$workspace" != "$FOCUSED_WORKSPACE" ]; then
-        echo "Focusing workspace $workspace"
-        i3-msg --socket $socket_path [workspace=$workspace] focus
-        break
-    fi
+    echo "Focusing workspace $workspace"
+    i3-msg --socket $socket_path [workspace=$workspace] focus
 done
 
-# Focus on the workspace that was focused before
-echo "Focusing workspace $FOCUSED_WORKSPACE"
-i3-msg --socket $socket_path [workspace=$FOCUSED_WORKSPACE] focus
+# Focus on the Monitor that was focused
+NEW_FOCUSED_WORKSPACE=$(i3-msg --socket $socket_path -t get_workspaces | jq -r '.[] | select(.output=="'$FOCUSED_MONITOR'") | select(.visible==true) | .name')
+
+echo "Focusing workspace $NEW_FOCUSED_WORKSPACE"
+i3-msg --socket $socket_path [workspace=$NEW_FOCUSED_WORKSPACE] focus
